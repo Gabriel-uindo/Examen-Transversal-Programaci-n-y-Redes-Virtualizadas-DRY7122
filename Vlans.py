13df9faf13b86fa03794bd2b3cd2aa14855e9171
Gabriel-uindo <gab.huenuman@duocuc.cl>
2025-07-10 11:13:05 -0400

    Delete Vlans.py

--- Vlans.py
+++
@@ -1,8 +0,0 @@
-vlan = int(input("Ingrese el número de VLAN: "))
-if 1 <= vlan <= 1005:
-    print("VLAN en rango normal.")
-elif 1006 <= vlan <= 4094:
-    print("VLAN en rango extendido.")
-else:
-    print("Número de VLAN fuera de rango válido.")
-
